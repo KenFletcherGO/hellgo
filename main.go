ef89a40f621b2ba37f38341d34b7ddea897e12df
KenFletcherGO <texas5starlanguageservices@gmail.com>
2019-10-02 15:06:00 -0500

    Initial Commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+
+	var greeting string = "Hola"
+	var languages = [6]string{"en", "es", "de", "fr", "jp", "ru"}
+
+	switch locale {
+
+	case "en":
+		greeting = "Hello"
+	case "es":
+		greeting = "Hola"
+	case "de":
+		greeting = "Guten Tag"
+	case "fr":
+		greeting = "Bon jour"
+	case "jp":
+		greeting = "Konnichiwa"
+	case "ru":
+		greeting = "Privet"
+	default:
+		greeting = "Yo!"
+
+	}
+
+}
